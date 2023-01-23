193969b0098044dfcbaa981c8c5658985da8bde5
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-01-23 14:52:06 +0100

    DOM manipulation

--- scripts/questions.js
+++ scripts/questions.js
@@ -7,3 +7,36 @@ let questions = [
     level: "beginner",
   },
 ];
+
+function prepareHTML(questions) {
+  "use strict";
+
+  let questionsHTMLArray = questions.map((item) => {
+    let questionHTML = `
+        <h2 class="question-text">${item.question}</h2>
+        <form>
+          <button type="button" class="answer-btn">${item.answers[0]}</button>
+          <br>
+          <button type="button" class="answer-btn">${item.answers[1]}</button>
+          <br>
+          <button type="button" class="answer-btn">${item.answers[2]}</button>
+          <br>
+          <button type="button" class="answer-btn">${item.answers[3]}</button>
+          <br>
+        </form>
+    `;
+
+    return questionHTML;
+  });
+
+  return questionsHTMLArray;
+}
+
+function displayQuestion(html) {
+  "use strict";
+
+  document.getElementById("question-container").innerHTML = html;
+}
+
+let questionsHTML = prepareHTML(questions);
+displayQuestion(questionsHTML[0]);
