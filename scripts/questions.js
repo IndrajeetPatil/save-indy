2ba3405bb57405033100460719408a10a596f9e4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-01-24 14:41:38 +0100

    more questions; add footer

--- scripts/questions.js
+++ scripts/questions.js
@@ -27,6 +27,20 @@ let questions = [
     language: "javascript",
     level: "beginner",
   },
+  {
+    question: "Which of the following is not a primitive type?",
+    answers: ["string", "object", "symbol", "undefined"],
+    correctAnswer: "object",
+    language: "javascript",
+    level: "beginner",
+  },
+  {
+    question: "Which of the following array methods doesn't create a new array?",
+    answers: ["filter", "pop", "map", "reduce"],
+    correctAnswer: "object",
+    language: "javascript",
+    level: "beginner",
+  },
 ];
 
 function prepareQuestions(questions) {
